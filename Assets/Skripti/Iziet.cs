2fd10bb207f006a8083c2a71fe7d528d6b996455
Daniels <d.jaunarajsjanvaris@gmail.com>
2024-06-11 01:19:49 +0300

    Pievienoju Iziet pogu

---
+++ Assets/Skripti/Iziet.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Iziet : MonoBehaviour
+{
+    public void apturet()
+    {
+        Application.Quit();
+    }
+}
